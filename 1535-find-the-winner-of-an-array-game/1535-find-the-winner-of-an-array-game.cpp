c64b9f39c9e832490cba53a2f21d684a5a0092b0
Ninad Sajwan <ninadmaster05@gmail.com>
2023-11-05 09:40:01 +0530

    Time: 99 ms (22.68%), Space: 63.6 MB (40.28%) - LeetHub

---
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int getWinner(vector<int>& arr, int k)
+    {
+        unordered_map<int,int> score;
+        
+        int n = arr.size();
+        int i = 0;
+        int j = 1;
+        
+        while(j < n)
+        {
+            if(arr[i] > arr[j])
+            {
+                score[arr[i]]++;
+                j++;
+                if(score[arr[i]] == k)
+                    return arr[i];
+            }
+            
+            else if(arr[j] > arr[i])
+            {
+                score[arr[j]]++;
+                if(score[arr[j]] == k)
+                    return arr[j];
+                i = j;
+                j++;
+            }
+            
+        }
+        
+        return arr[i];
+        
+    }
+};
