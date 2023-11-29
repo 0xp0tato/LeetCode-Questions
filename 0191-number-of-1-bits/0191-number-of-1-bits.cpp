f1423200c0e4eabb6e13e0d8bdda8e117be8068d
Ninad Sajwan <ninadmaster05@gmail.com>
2023-11-29 13:00:41 +0530

    Time: 4 ms (30.61%), Space: 6.4 MB (27.30%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -2,7 +2,13 @@ class Solution {
 public:
     int hammingWeight(uint32_t n)
     {
-        return __builtin_popcount(n);
+        int ans = 0;
+        while(n)
+        {
+            ans = (n & 1) ? ans + 1 : ans + 0;
+            n = n >> 1;
+        }
         
+        return ans;
     }
 };
