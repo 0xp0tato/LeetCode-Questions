c997374713973dcc94a0d0b51a5e61d3766aef6f
Ninad Sajwan <ninadmaster05@gmail.com>
2024-05-04 08:20:13 +0530

    Time: 38 ms (98.90%), Space: 45.4 MB (46.86%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit)
     {
+        std::ios_base::sync_with_stdio(false);
+	    std::cin.tie(nullptr);
         int i = 0;
         int j = people.size() - 1;
         
