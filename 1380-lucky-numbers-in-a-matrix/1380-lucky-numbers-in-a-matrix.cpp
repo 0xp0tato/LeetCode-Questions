1faedd89367ab406fbbd60aa1585696ad347e377
Ninad Sajwan <ninadmaster05@gmail.com>
2024-07-19 21:21:15 +0530

    Time: 16 ms (56.65%), Space: 14.7 MB (10.51%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
@@ -2,40 +2,30 @@ class Solution {
 public:
     vector<int> luckyNumbers (vector<vector<int>>& matrix)
     {
-        vector<int> ans;
+        unordered_map<int,int> minis;
         
         int m = matrix.size();
         int n = matrix[0].size();
         
         for(int i=0;i<m;i++)
         {
-            int col = 0;
-            int mini = INT_MAX;
+            int mn = *min_element(matrix[i].begin(), matrix[i].end());
+            minis[mn]++;
+        }
+        
+        vector<int> ans;
+        
+        for(int i=0;i<n;i++)
+        {
+            int mx = INT_MIN;
+            for(int j=0;j<m;j++)
+                mx = max(mx, matrix[j][i]);
             
-            // find minimum number in the row
-            for(int j=0;j<n;j++)
-                if(matrix[i][j] < mini)
-                {
-                    mini = matrix[i][j];
-                    col = j;
-                }
-            
-            // check if that number is max in the column
-            
-            if(checkMax(col, m, mini, matrix))
-                ans.push_back(mini);
+            if(minis.find(mx) != minis.end())
+                ans.push_back(mx);
         }
         
         return ans;
         
-    }
-    
-    bool checkMax(int j, int m, int mini, vector<vector<int>> matrix)
-    {
-        for(int k=0;k<m;k++)
-            if(matrix[k][j] > mini)
-                return false;
-        
-        return true;
     }
 };
