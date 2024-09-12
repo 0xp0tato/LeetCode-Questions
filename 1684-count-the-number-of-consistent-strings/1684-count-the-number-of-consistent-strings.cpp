47c92eec89fb56c31319e83580f1b5656ba271ab
Ninad Sajwan <ninadmaster05@gmail.com>
2024-09-12 09:31:37 +0530

    Time: 46 ms (61.36%), Space: 34 MB (27.48%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
@@ -3,27 +3,24 @@ public:
     int countConsistentStrings(string allowed, vector<string>& words)
     {
         unordered_map<int, int> freq;
-        int ans = 0;
+        int cnt = 0;
         
         for(char c: allowed)
             freq[c]++;
         
         for(string s: words)
         {
-            bool flag = true;
             for(char c: s)
             {
                 if(freq.find(c) == freq.end())
                 {
-                    flag = false;
+                    cnt++;
                     break;
                 }
             }
-            
-            if (flag) ans++;
         }
         
-        return ans;
+        return words.size() - cnt;
         
     }
 };
